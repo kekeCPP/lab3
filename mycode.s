5580ed9b70bf5e61e35bd93164fa8a04f23f4685
Hampus Grimskär <hampus.grimskar@gmail.com>
2022-12-21 20:21:30 +0100

    Final

--- mycode.s
+++ mycode.s
@@ -1,6 +1,4 @@
     .data
-headMsg:	.asciz	"44Start av testprogram. Skriv in 5 tal!"
-format:     .ascii  "%c\n"
 buf:        .space  64
 inbuff:     .space  64
 outbuff:    .space  64
@@ -14,6 +12,7 @@ outbuffpos: .quad   0
     .global getInPos
     .global getChar
     .global getText
+    .global getText2
     .global getOutPos
     .global setOutPos
     .global outImage
@@ -30,11 +29,16 @@ inImage:
     ret
 
 getInt:
-    movq    $0, %rax
+    call    getInPos
     movq    $inbuff, %rdi
-loop:
+    addq    %rax, %rdi      # %rdi = pointer to current position of inbuff
+    movq    $0, %rax
+
+lIgnoreSpaces:
     cmpb    $' ', (%rdi) # Compare character to blankspace
     je      blankspace
+
+loop:
     cmpb    $'+', (%rdi) # Compare character to +
     je      blankspace
     cmpb    $'-', (%rdi) # Compare character to -
@@ -42,33 +46,52 @@ loop:
     cmpb    $'0', (%rdi)
     jl      end
     cmpb    $'9', (%rdi)
-    jge     end
+    jg      end
     movzbq  (%rdi), %r10
     subq    $'0', %r10
     imulq   $10, %rax
     addq    %r10, %rax
     incq    %rdi
     jmp     loop
+
 blankspace:     # If blank space or + then go to next character
     incq    %rdi
-    jmp     loop
+    jmp     lIgnoreSpaces
+
 negative:
     movzbq  (%rdi), %r11 # Put the - sign in a register
     incq    %rdi
     jmp     loop
+
 negate:
     neg     %rax # Negate the number
     movq    $'/', %r11 # change the character in %r11 so we dont get an infinate loop
+
 end:
     cmpq    $'-', %r11
-    je      negate
+    je      negate          # negate the number if there is a - sign in %r11
+    subq    $inbuff, %rdi   # %rdi = the new position of inbuff
+    call    setInPos        # set the new position
     ret
 
 
 setInPos:
     #### Parameters ####
-    # %rdi = index to set position to
-    movq    %rdi, inbuffpos
+    # %edi = index to set position to
+    movslq  %edi, %rdi      # sign extended 4-byte to 8-byte
+    cmpq    $0, %rdi        # check if input < 0
+    jl      jSetPosZero     # if yes set pos to 0
+    cmpq    $63, %rdi       # check if input > 63
+    jg      jSetPosMax      # if yes set pos to 63
+    movq    %rdi, inbuffpos # else set pos to input
+    ret
+
+jSetPosZero:
+    movq    $0, inbuffpos
+    ret
+
+jSetPosMax:
+    movq    $63, inbuffpos
     ret
 
 
@@ -85,12 +108,10 @@ getChar:
     movq    $inbuff, %rdi
     call    getInPos        # get the current position of inbuff
 
-    # cmpq    $0, %rax
-    # je      cInImage
     cmpq    $0, (%rdi)     # Check if inbuff is empty
     je      cInImage
     cmpq    $63, %rax       # Check if we are at the last position of inbuff
-    je      cInImage
+    jg      cInImage
 
 fGetChar:
     movq    %rax, %r10      # store the value from getInPos for later use
@@ -114,45 +135,51 @@ getText:
     # %rdi = buf
     # %rsi = number of characters to read
     # %rax = number of characters transferred
-fGetText:
-    movq    $inbuff, %rdx
-    call    getInPos
+
+    movq    %rsi, %rdx      # %rdx = number of characters to read
+    call    getInPos        # %rax = current position of inbuff
+    movq    $inbuff, %rsi   # %rsi = pointer to outbuff
+    addq    %rax, %rsi      # %rsi = pointer to current position in inbuff
+    movq    $0, %rcx        # %rcx = characters read counter
+    pushq   %rdi            # push initial value of %rdi to stack
+
+lGetText:
+    cmpq    %rcx, %rdx      
+    je      rGetText2       # if chracters read = number of characters to read
     cmpq    $63, %rax
-    je      cInImage2
-    cmpq    $0, (%rdx)
-    je      cInImage2
+    jg      cGetText
+    cmpb    $0, (%rsi)      # compare character in buf to null
+    je      rGetText2       # if null return
+    movq    (%rsi), %r10    # move character to temporary register
+    movq    %r10, (%rdi)    # move character from temporary register to current position in inbuff
+    incq    %rsi            # current position in inbuff++
+    incq    %rax            # new position in inbuff++
+    incq    %rdi            # position in buf++
+    incq    %rcx            # characters read++
+    jmp     lGetText        # read the next character
 
-
-    movq    $0, %rcx
-    addq    %rax, %rcx
-    cmpq    $0, %rsi
-    jg      lReadChar
-    movq    %rcx, %rax
-    ret
-
-lReadChar:
-    cmpq    $63, %rcx
-    jg      rGetText        # Return if we are at the end of the buffer
-    cmpq    $0, (%rdx)
-    je      rGetText        # Return if there is nothing more to read
+rGetText2:
+    # movq    (%rsi), %r10    # move the null character to temporary register
+    # movq    %r10, (%rdi)    # move the null character from temporary register to current position in inbuff
+    pushq   %rdi            # push value in %rdi to stack because we need to change it for setInPos
+    # incq    %rax            # new position in inbuff++
+    movq    %rax, %rdi      # %rdi = new position in inbuff
+    call    setInPos        # set the new position in the inbuff
     
-    addq    %rcx, %rdx      # [inbuff + number of characters transferred]
-    addq    %rcx, %rdi
-    movq    (%rdx), %rdi    # buf[number of characters transferred] = inbuff[number of characters transferred]
-    incq    %rcx            # number of characters transferred++
-    
-    cmpq    %rcx, %rsi
-    jg      lReadChar       # while number of characters to read > number of characters transferred
-    movq    %rcx, %rax
+    popq    %rdi            # pop the value of %rdi that we just pushed
+    popq    %rsi            # pop the initial value of %rdi from the stack and store it in %rsi 
+    subq    %rsi, %rdi      # current position in buf - start of buf. %rdi = number of characters transferred
+    movq    %rdi, %rax      # return the amount of characters transferred     
     ret
 
-rGetText:
-    movq    %rcx, %rax
-    ret
-
-cInImage2:
+cGetText:
+    subq    %rax, %rsi      # reset the outbuff pointer back to the first position
+    pushq   %rdi            # push the pointer in %rdi to the stack since outImage will change it
     call    inImage
-    jmp     fGetText
+    call    getInPos        # reset %rax position back to 0
+    popq    %r11            # restore the pointer to the current position in buf from the stack into %r11
+    movq    %r11, %rdi      # restore the pointer to the current position of buf to %rdi
+    jmp     lGetText
 
 
 getOutPos:
@@ -165,33 +192,52 @@ getOutPos:
 
 setOutPos:
     #### Parameters ####
-    # %rdi = index to set position to
+    # %edi = index to set position to
 
-    movq    %rdi, outbuffpos
+    movslq  %edi, %rdi          # sign extend 4-byte to 8-byte
+    cmpq    $0, %rdi            # check if input < 0
+    jl      jSetOposZero        # if yes set pos to 0
+    cmpq    $63, %rdi           # check if input > 63
+    jg      jSetOposMax         # if yes set pos to 63
+
+    movq    %rdi, outbuffpos    # else set pos to input
+    ret
+
+jSetOposZero:
+    movq    $0, outbuffpos
+    ret
+
+jSetOposMax:
+    movq    $63, outbuffpos
     ret
 
 outImage:
     movq    $outbuff, %rdi
-    # movq    $64, %rsi
-    # movq    stdout, %rdx
     call    puts
     movq    $0, %rdi
     call    setOutPos
-    # movq    $0, outbuffpos
     ret
 
 putInt:
     #### Parameters ####
-    # %rdi = number to put in outbuff
+    # %edi = number to put in outbuff
+
 
     call    getOutPos
-    movq    %rax, %r11      # %r10 = current position of outbuff
+    movq    %rax, %r11      # %r11 = current position of outbuff
     movq    $outbuff, %r10  # %r10 = pointer to outbuff
     addq    %r11, %r10      # %r10 = pointer to current position in outbuff
-    movq    %rdi, %rax      # result
+    movslq  %edi, %rax      # move the 4-byte integer from the input into an 8-byte register and keep sign
     movq    $0, %rdi        # counter of numbers added
     movq    $10, %rbx       # divisor
     movq    $0, %rdx        # remainder
+
+
+    cmpq    $0, %rax        # Check if number is negative or not
+    jge     lPutInt         # if number >= 0 dont do anything
+    negq    %rax            # else make it positive and add a '-' sign to the string
+    movb    $'-', (%r10)
+    incq    %r10
 
     
 lPutInt:
@@ -201,7 +247,7 @@ lPutInt:
     pushq   %rdx            # push ascii character to stack
     incq    %rdi            # %rdi++
     cmpq    $0, %rax
-    jg      lPutInt         # loop if the quotient is larger than 0 (there are more numbers to read)
+    jnz     lPutInt         # loop if the quotient is larger than 0 (there are more numbers to read)
     movq    %rdi, %r11      # else save the counter in %r11 for later use     
 
 rPutInt:     
@@ -209,12 +255,13 @@ rPutInt:
     incq    %r10            # %r10++
     dec     %rdi            # decrement the counter of numbers to add (%rdi--)
     cmpq    $0, %rdi
-    jg      rPutInt         # jump if there are still more numbers to pop from the stack
+    jnz      rPutInt         # jump if there are still more numbers to pop from the stack
 
     movq    $0, (%r10)      # else add a null character at the end of the string
-    addq    $1, %r11        # increment the counter of characters added
-    # movq    $outbuff, %rax  # move outbuff to the return register (unnecessary because we don't care about return value from this function)
-    movq    %r11, %rdi  
+    # addq    $1, %r11        # increment the counter of characters added
+    # movq    $outbuff, %rax  # move outbuff to the return register (unnecessary because we don't care about return value from this function) 
+    subq    $outbuff, %r10  # get the new postition of outbuff
+    movq    %r10, %rdi      
     call    setOutPos       # set the new position in outbuff to outbuffpos + amount of numbers added + 1 for null character
     ret                     # return
 
@@ -228,8 +275,8 @@ putText:
     addq    %rax, %rsi      # %rsi = pointer to current position in outbuff
 
 lPutText:
-    cmpq    $64, %rax
-    je      cPutText
+    cmpq    $63, %rax
+    jg      cPutText
     cmpb    $0, (%rdi)      # compare character in buf to null
     je      rPutText        # if null return
     movq    (%rdi), %r10    # move character to temporary register
@@ -240,9 +287,9 @@ lPutText:
     jmp     lPutText        # read the next character
 
 rPutText:
-    movq    (%rdi), %r10    # move the null character to temporary register
-    movq    %r10, (%rsi)    # move the null character from temporary register to current position in outbuff
-    incq    %rax            # new position in outbuff++
+    # movq    (%rdi), %r10    # move the null character to temporary register
+    # movq    %r10, (%rsi)    # move the null character from temporary register to current position in outbuff
+    # incq    %rax            # new position in outbuff++
     movq    %rax, %rdi      # %rdi = new position in outbuff
     call    setOutPos       # set the new position in the outbuff
     ret
--- test.c
+++ test.c
@@ -1,38 +1,79 @@
 #include <stdio.h>
 
 void inImage();
-int getInt(char*);
-int getText(char buf[], int n);
+int getInt();
+int getText(char* buf, int n);
+int getText2(char* buf, int n);
 void setInPos(int pos);
 int getInPos();
 char getChar();
-char* putInt(int a);
-int putText(char* string);
+void putInt(int a);
+void putText(char* string);
 void setOutPos(int pos);
-int putChar(char a);
+int getOutPos();
+void putChar(char a);
+void outImage();
 
 int main()
 {
-    // inImage();
+    char* test = "THIS IS A TEST PROMPT FOR DEBUGGING";
+    char* headMsg = "Start av testprogram. Skriv in 5 tal!";
+    char* endMsg = "Slut på testprogrammet";
+    char buf[64];
+    int sum = 0;
+    int count = 0;
+    int temp = 0;
+    int rdi;
+    int rdx;
+    int rsi;
+    int rax;
 
-    // int a;
-    // setInPos(0);
-    // a = getInPos();
-    // printf("INPOS: %d\n", a);
 
-    // char b;
-    // b = getChar();
-    // printf("RETURNED CHARACTER: %c\n", b);
-    // a = getInPos();
-    // printf("INPOS: %d\n", a);
+    putText(headMsg);
+    outImage();
+    inImage();
+    count = 5;
 
-    // char buf[64];
-    // a = getText(buf, 4);
-    // printf("GET TEXT TRANSFERRED %d CHARACTERS\n", a);
-    // int a = putText("hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh");
-    // printf("THE STRING IS: %d\n", a);
-    // int b = putChar('g');
-    // printf("THE POSITION IS: %d\n", b);
+    while (count > 0)
+    {
+        rax = getInt();
+        temp = rax;
+        if (rax < 0)
+        {
+            rax = getOutPos();
+            rax--;
+            rdi = rax;
+            setOutPos(rdi);
+        }
+
+        rdx = temp;
+        sum = sum + rdx;
+        rdi = rdx;
+        printf("RDI = %d\n", rdi);
+        printf("OUTPOS: %d\n", getOutPos());
+        putInt(rdi);
+        rdi = 43;
+        putChar('+');
+        count--;
+    }
+
+    rax = getOutPos();
+    rax--;
+    rdi = rax;
+    setOutPos(rdi);
+    rdi = 61;
+    putChar('=');
+    rdi = sum;
+    putInt(rdi);
+    outImage();
+    rsi = 12;
+    getText(buf, rsi);
+    putText(buf);
+    rdi = 125;
+    putInt(rdi);
+    outImage();
+    putText(endMsg);
+    outImage();
 
     return 0;
 }
